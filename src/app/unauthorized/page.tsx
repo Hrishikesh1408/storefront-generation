ae22e8b833c4cef60ca0d21e5c0e0ec3391fbbe7
hrishikeshe <Hrishikesh.Eswaran@infinite.com>
2026-03-18 16:35:23 +0530

    Proxy for secure routing

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export default function Unauthorized() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-semibold">
+        You are not authorized to access this page.
+      </h1>
+    </div>
+  );
+}
---
+++ src/proxy.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const role = request.cookies.get("role")?.value;
+  const path = request.nextUrl.pathname;
+
+  console.log("PROXY HIT:", path, role);
+
+  if (path.startsWith("/admin") && role !== "admin") {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  if (path.startsWith("/merchant") && role !== "merchant") {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*", "/merchant/:path*"],
+};
